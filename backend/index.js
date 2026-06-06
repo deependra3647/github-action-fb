a411d56512601bc3d3ce1ebcd5012b8ebf5586f6
Deependra <deependrakansana2004@gmail.com>
2026-06-06 23:59:22 +0530

    initial-commit

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello from Backend"
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Backend running on port 5000");
+});
